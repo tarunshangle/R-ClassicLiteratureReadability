8833f02a03d4753fccb68686beceb94736827ab9
Tom Bell <tjbell@gmail.com>
2019-04-26 15:17:45 -0600

    chore: remove solution

--- tests/solution.R
+++ tests/solution.R
@@ -1,4 +1,14 @@
-source('data.R')
+library(readr)
+library(dplyr)
+library(stringr)
+library(ggplot2)
+library(tidyr)
+
+titles <- read_csv('data/titles.csv')
+stats <- read_csv('data/stats.csv')
+
+books <- full_join(titles, stats) %>%
+  filter(!str_detect(title, 'Project Gutenberg'))
 
 books <- books %>%
   arrange(desc(downloads)) %>%
--- tests/tests_module1.R
+++ tests/tests_module1.R
@@ -1,14 +1,17 @@
 library(rlang)
+library(glue)
 
 setwd('..')
 
 solution <- new.env()
 source('tests/solution.R', local = solution)
 
-user <- new.env()
-source('reading.R', local = user)
+user_source_file <- 'reading.R'
 
-parsed <- parse_exprs(file('time.R'))
+user <- new.env()
+source(user_source_file, local = user)
+
+parsed <- parse_exprs(file(user_source_file))
 
 for (line in parsed) {
   if(line[[1]] == 'plot' && line[[2]] == 'p') {
@@ -18,35 +21,35 @@ for (line in parsed) {
 
 context('Module 01')
 test_that('Require the readr package. @readr-package', {
-  expect(exists('readr_package'), 'Have you required the `readr` package in `time.R` with the `library()` function?')
+  expect(exists('readr_package'), glue('Have you required the `readr` package in `{user_source_file}` with the `library()` function?'))
 })
 
 test_that('Read the titles data. @read-csv-titles', {
-  expect('titles' %in% ls(envir = user), 'Does the `titles` data frame exist in `time.R`?')
+  expect('titles' %in% ls(envir = user), glue('Does the `titles` data frame exist in `{user_source_file}`?'))
   expect(isTRUE(all_equal(user$titles, solution$titles)), 'Are you using the `read_csv()` function from the `readr` package to read in the `titles.csv` file?')
 })
 
 test_that('Read the stats data. @read-csv-stats', {
-  expect('stats' %in% ls(envir = user), 'Does the `stats` data frame exist in `time.R`?')
+  expect('stats' %in% ls(envir = user), glue('Does the `stats` data frame exist in `{user_source_file}`?'))
   expect(isTRUE(all_equal(user$stats, solution$stats)), 'Are you using the `read_csv()` function from the `readr` package to read in the `stats.csv` file?')
 })
 
 test_that('Require the dplyr package. @dplyr-package', {
-  expect(exists('dplyr_package'), 'Have you required the `dplyr` package in `time.R` with the `library()` function?')
+  expect(exists('dplyr_package'), glue('Have you required the `dplyr` package in `{user_source_file}` with the `library()` function?'))
 })
 
 test_that('Join titles and stats. @join-titles-stats', {
-  expect('books' %in% ls(envir = user), 'Does the `books` data frame exist in `time.R`?')
+  expect('books' %in% ls(envir = user), glue('Does the `books` data frame exist in `{user_source_file}`?'))
   expect(isTRUE(all_equal(user$books, solution$books)), 'Are you joining the `titles` and `stats` data frames with `full_join()`?')
 })
 
 test_that("Find Dickens' Works @filter-data", {
-  expect('dickens' %in% ls(envir = user), 'Does the `dickens` data frame exist in `time.R`?')
+  expect('dickens' %in% ls(envir = user), glue('Does the `dickens` data frame exist in `{user_source_file}`?'))
   expect(isTRUE(all_equal(user$dickens, solution$dickens)), 'Does the `dickens` data frame only contain his works?')
 })
 
 test_that('Refining Columns. @refine-columns', {
-  expect('dickens_stats' %in% ls(envir = user), 'Does the `dickens_stats` data frame exist in `time.R`?')
+  expect('dickens_stats' %in% ls(envir = user), glue('Does the `dickens_stats` data frame exist in `{user_source_file}`?'))
   correct_names <- c("id", "words", "sentences", "to_be_verbs", "contractions", "pauses", "cliches", "similes")
   user_names <- names(user$dickens_stats)
   expect(identical(correct_names, user_names), 'The column names do not match?')
@@ -54,26 +57,26 @@ test_that('Refining Columns. @refine-columns', {
 })
 
 test_that('Importing Year Published. @read-csv-published', {
-  expect('published' %in% ls(envir = user), 'Does the `published` data frame exist in `time.R`?')
+  expect('published' %in% ls(envir = user), glue('Does the `published` data frame exist in `{user_source_file}`?'))
   expect(isTRUE(all_equal(user$published, solution$published)), 'The `published` data frame should have an `id` and a `year` column.')
 })
 
 test_that('Joining year published. @join-dickens-published', {
-  expect('time' %in% ls(envir = user), 'Does the `time` data frame exist in `time.R`?')
+  expect('time' %in% ls(envir = user), glue('Does the `time` data frame exist in `{user_source_file}`?'))
   expect(isTRUE(all_equal(user$time, solution$time)), 'Are you joining the `dickens_stats` and `published` data frames with `full_join()`?')
 })
 
 test_that('Require the tidyr package. @tidyr-package', {
-  expect(exists('tidyr_package'), 'Have you required the `tidyr` package in `time.R` with the `library()` function?')
+  expect(exists('tidyr_package'), glue('Have you required the `tidyr` package in `{user_source_file}` with the `library()` function?'))
 })
 
 test_that('Reshaping data frames. @reshape', {
-  expect('time_long' %in% ls(envir = user), 'Does the `time_long` data frame exist in `time.R`?')
+  expect('time_long' %in% ls(envir = user), glue('Does the `time_long` data frame exist in `{user_source_file}`?'))
   expect(isTRUE(all_equal(user$time_long, solution$time_long)), 'Are you using the `gather()` function to reshape the `time` data frame?')
 })
 
 test_that('Require the ggplot2 package. @ggplot2-package', {
-  expect(exists('ggplot2_package'), 'Have you required the `ggplot2` package in `time.R` with the `library()` function?')
+  expect(exists('ggplot2_package'), glue('Have you required the `ggplot2` package in `{user_source_file}` with the `library()` function?'))
 })
 
 test_that('Initialize a Plot Object. @ggplot', {
